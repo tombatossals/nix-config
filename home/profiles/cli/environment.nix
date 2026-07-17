5501b503d587d895fc696d694b5f383734f799b2
David Rubert <david.rubert@gmail.com>
2026-07-17 19:56:33 +0200

    up

--- home/profiles/cli/environment.nix
+++ home/profiles/cli/environment.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   home.sessionVariables = {
@@ -9,5 +9,17 @@
     LESS = "-FR";
 
     MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+
+    LD_LIBRARY_PATH = "${config.home.profileDirectory}/lib";
+    NODE_PATH = "${config.home.homeDirectory}/.npm-packages/lib/node_modules";
+
   };
+
+  home.sessionPath = [
+    "/opt/sqlcl/bin"
+    "${config.home.homeDirectory}/.venv/bin"
+    "${config.home.homeDirectory}/.npm-packages/bin"
+    "${config.home.homeDirectory}/.npm-global/bin"
+    "${config.home.homeDirectory}/.local/bin"
+  ];
 }
