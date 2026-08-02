7d3ad2c666b236fcb7683397b2aeaaafad96bb3b
David Rubert <david.rubert@gmail.com>
2026-08-02 09:07:06 +0200

    up

--- home/profiles/dev/default.nix
+++ home/profiles/dev/default.nix
@@ -2,15 +2,16 @@
 
 {
   imports = [
+    ./cpp.nix
     ./direnv.nix
     ./database.nix
-    ./cpp.nix
     ./go.nix
-    ./herdr.nix
-    ./node.nix
-    ./python.nix
-    ./rust.nix
+    #./herdr.nix
     ./lazyvim.nix
     ./nginx.nix
+    ./node.nix
+    ./opencode.nix
+    ./python.nix
+    ./rust.nix
   ];
 }
